9bf34fa576118ac26047aafcb92d62fa86cefda7
Helgi Tomas Gislason <helgso@gmail.com>
2019-01-26 14:50:32 -0500

    Kaggle: Encapsulating the logic in cifar_cnn inside a main function

--- kaggle/cifar_cnn.py
+++ kaggle/cifar_cnn.py
@@ -11,187 +11,193 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from sklearn.metrics import classification_report
 import kaggle.neuralnets as neuralnets
 
-# Device configuration
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-print("Using device:", device)
-# Hyper parameters
-num_epochs = 50
-num_classes = 10
-batch_size = 100
-learning_rate = 0.001
-input_dropout_rate = 0
-weight_decay = 0.001 #As in the paper
+def main():
+    # Device configuration
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# MNIST dataset
-train_dataset = torchvision.datasets.CIFAR10(root='../../data/',
-                                            train=True, 
-                                            transform=transforms.Compose([
-                                                transforms.RandomHorizontalFlip(),
-                                                transforms.RandomResizedCrop(32, scale=(0.85,1.0)),
-                                                transforms.RandomRotation(10),
-                                                transforms.ToTensor(),
-                                                ]),
-                                            download=True)
+    print("Using device:", device)
+    # Hyper parameters
+    num_epochs = 50
+    num_classes = 10
+    batch_size = 100
+    learning_rate = 0.001
+    input_dropout_rate = 0
+    weight_decay = 0.001 #As in the paper
 
-test_dataset = torchvision.datasets.CIFAR10(root='../../data/',
-                                          train=False, 
-                                          transform=transforms.ToTensor())
+    # MNIST dataset
+    train_dataset = torchvision.datasets.CIFAR10(root='../../data/',
+                                                train=True,
+                                                transform=transforms.Compose([
+                                                    transforms.RandomHorizontalFlip(),
+                                                    transforms.RandomResizedCrop(32, scale=(0.85,1.0)),
+                                                    transforms.RandomRotation(10),
+                                                    transforms.ToTensor(),
+                                                    ]),
+                                                download=True)
 
-
-n_train = 10000
-indices = list(range(len(train_dataset)))
-random.shuffle(indices)
+    test_dataset = torchvision.datasets.CIFAR10(root='../../data/',
+                                              train=False,
+                                              transform=transforms.ToTensor())
 
 
-# Data loader
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size, 
-                                           shuffle=False,
-                                           sampler=SubsetRandomSampler(indices[:n_train]))
-                                        
-validation_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size, 
-                                           shuffle=False,
-                                           sampler=SubsetRandomSampler(indices[n_train:]))
-
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size, 
-                                          shuffle=False)
+    n_train = 10000
+    indices = list(range(len(train_dataset)))
+    random.shuffle(indices)
 
 
+    # Data loader
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=False,
+                                               sampler=SubsetRandomSampler(indices[:n_train]))
+
+    validation_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=False,
+                                               sampler=SubsetRandomSampler(indices[n_train:]))
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False)
 
 
-model = neuralnets.CifarNet(num_classes).to(device)
 
-def print_score(which, predicted, labels):
-    print(which + "Score:")
-    print(which + "Score:",file=logfile)
-    print(classification_report(predicted, labels, target_names=target_names))
-    print(classification_report(predicted, labels, target_names=target_names), file=logfile)
 
-#Logging functions. Will be used later for ploting graphs
-import time
-import datetime
-logfile_prefix = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H_%M_%S')
+    model = neuralnets.CifarNet(num_classes).to(device)
 
-logfile = open("results/" + logfile_prefix + ".txt","w+")
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-# TODO: Don't use Adam(assignment specifications don't allow using it)
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    def print_score(which, predicted, labels):
+        print(which + "Score:")
+        print(which + "Score:",file=logfile)
+        print(classification_report(predicted, labels, target_names=target_names))
+        print(classification_report(predicted, labels, target_names=target_names), file=logfile)
 
-print(model, file=logfile)
-print(optimizer, file=logfile)
-print("Learning rate:", learning_rate, file=logfile)
-logfile.flush()
-target_names = ["plane","auto", "bird", "cat",  "deer",  "dog", "frog", "horse", "ship",   "truck"]
+    #Logging functions. Will be used later for ploting graphs
+    import time
+    import datetime
+    logfile_prefix = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H_%M_%S')
 
-scheduler = MultiStepLR(optimizer, milestones=[20,40], gamma=0.1)
+    logfile = open("results/" + logfile_prefix + ".txt","w+")
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    # TODO: Don't use Adam(assignment specifications don't allow using it)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-# Train the model
-total_step = len(train_loader)
+    print(model, file=logfile)
+    print(optimizer, file=logfile)
+    print("Learning rate:", learning_rate, file=logfile)
+    logfile.flush()
+    target_names = ["plane","auto", "bird", "cat",  "deer",  "dog", "frog", "horse", "ship",   "truck"]
 
-for epoch in range(num_epochs):
-    #Reset metrics for each epoch
-    scheduler.step()
-    full_train_predicted = []
-    full_train_labels = []
-    full_validation_predicted = []
-    full_validation_labels = []
+    scheduler = MultiStepLR(optimizer, milestones=[20,40], gamma=0.1)
 
-    correct = 0
-    total = 0
-    validation_total = 0
-    validation_correct = 0
-    for i, (images, labels) in enumerate(train_loader):
-        images = images.to(device)
-        labels = labels.to(device)
-        
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        total += labels.size(0)
-        _, predicted = torch.max(outputs.data, 1)
-        full_train_predicted+=predicted.cpu().numpy().tolist()
-        full_train_labels+=labels.cpu().numpy().tolist()
+    # Train the model
+    total_step = len(train_loader)
 
-        correct += (predicted == labels).sum().item()
-        
-        if (i+1) % (batch_size/2) == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy:{:.2f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item(), correct/total), file=logfile)
-            logfile.flush()
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy:{:.2f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item(), correct/total))
-    print_score("Training", full_train_predicted, full_train_labels)
-    #At the end of the epoch, perform validation.
-    if (epoch%10==0):
-        torch.save(model, "model.bak")
-        #model.save("model.bak") #Model backup at each 10 epoch, just in case.
+    for epoch in range(num_epochs):
+        #Reset metrics for each epoch
+        scheduler.step()
+        full_train_predicted = []
+        full_train_labels = []
+        full_validation_predicted = []
+        full_validation_labels = []
 
-    with torch.no_grad():
-        validation_correct = 0
+        correct = 0
+        total = 0
         validation_total = 0
-        for images, labels in validation_loader:
-            validation_images = images.to(device)
-            validation_labels = labels.to(device)
-            validation_outputs = model(validation_images)
-            _, validation_predicted = torch.max(validation_outputs.data, 1)
-            validation_total += validation_labels.size(0)
-            validation_correct += (validation_predicted == validation_labels).sum().item()
-            full_validation_predicted+=validation_predicted.cpu().numpy().tolist()
-            full_validation_labels+=validation_labels.cpu().numpy().tolist()
-        print_score("Validation", full_validation_predicted, full_validation_labels)
-        print ("Epoch {} validation accuracy= {:.4f}".format(epoch+1, validation_correct/validation_total))
-        print ("Epoch {} validation accuracy= {:.4f}".format(epoch+1, validation_correct/validation_total), file=logfile)
-        
-# Test the model
-model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+        validation_correct = 0
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
 
-w, h = 10,10
-confusion_matrix = [[0 for x in range(w)] for y in range(h)] 
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total += labels.size(0)
+            _, predicted = torch.max(outputs.data, 1)
+            full_train_predicted+=predicted.cpu().numpy().tolist()
+            full_train_labels+=labels.cpu().numpy().tolist()
+
+            correct += (predicted == labels).sum().item()
+
+            if (i+1) % (batch_size/2) == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy:{:.2f}'
+                       .format(epoch+1, num_epochs, i+1, total_step, loss.item(), correct/total), file=logfile)
+                logfile.flush()
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy:{:.2f}'
+                       .format(epoch+1, num_epochs, i+1, total_step, loss.item(), correct/total))
+        print_score("Training", full_train_predicted, full_train_labels)
+        #At the end of the epoch, perform validation.
+        if (epoch%10==0):
+            torch.save(model, "model.bak")
+            #model.save("model.bak") #Model backup at each 10 epoch, just in case.
+
+        with torch.no_grad():
+            validation_correct = 0
+            validation_total = 0
+            for images, labels in validation_loader:
+                validation_images = images.to(device)
+                validation_labels = labels.to(device)
+                validation_outputs = model(validation_images)
+                _, validation_predicted = torch.max(validation_outputs.data, 1)
+                validation_total += validation_labels.size(0)
+                validation_correct += (validation_predicted == validation_labels).sum().item()
+                full_validation_predicted+=validation_predicted.cpu().numpy().tolist()
+                full_validation_labels+=validation_labels.cpu().numpy().tolist()
+            print_score("Validation", full_validation_predicted, full_validation_labels)
+            print ("Epoch {} validation accuracy= {:.4f}".format(epoch+1, validation_correct/validation_total))
+            print ("Epoch {} validation accuracy= {:.4f}".format(epoch+1, validation_correct/validation_total), file=logfile)
+
+    # Test the model
+    model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+
+    w, h = 10,10
+    confusion_matrix = [[0 for x in range(w)] for y in range(h)]
 
 
-full_test_predicted = []
-full_test_labels = []
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        predicted = predicted.cpu().numpy().tolist()
-        labels = labels.cpu().numpy().tolist()
-        full_test_predicted+=predicted
-        full_test_labels+=labels
-        #Builds the confusion matrix
-        for prediction, target in zip(predicted,labels):
-            confusion_matrix[prediction][target]+=1
-    print_score("Test", full_test_predicted, full_test_labels)
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total), file=logfile)
-    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+    full_test_predicted = []
+    full_test_labels = []
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            predicted = predicted.cpu().numpy().tolist()
+            labels = labels.cpu().numpy().tolist()
+            full_test_predicted+=predicted
+            full_test_labels+=labels
+            #Builds the confusion matrix
+            for prediction, target in zip(predicted,labels):
+                confusion_matrix[prediction][target]+=1
+        print_score("Test", full_test_predicted, full_test_labels)
+        print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total), file=logfile)
+        print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
 
-# Save the model checkpoint
-torch.save(model.state_dict(), 'model.ckpt')
+    # Save the model checkpoint
+    torch.save(model.state_dict(), 'model.ckpt')
 
-np.savetxt("results\\"+logfile_prefix+"_confusion_matrix.txt",np.matrix(confusion_matrix))
+    np.savetxt("results\\"+logfile_prefix+"_confusion_matrix.txt",np.matrix(confusion_matrix))
 
-#Just for the log
-print("Predicted (row) labels vs targets (column)", file=logfile)
-for i in range(0,10):
-    for j in range(0,10):
-        print(confusion_matrix[i][j],"\t",end='', file=logfile)
-    print("\n",end="", file=logfile)
+    #Just for the log
+    print("Predicted (row) labels vs targets (column)", file=logfile)
+    for i in range(0,10):
+        for j in range(0,10):
+            print(confusion_matrix[i][j],"\t",end='', file=logfile)
+        print("\n",end="", file=logfile)
 
 
-logfile.close()
+    logfile.close()
+
+
+if __name__ == '__main__':
+    main()
