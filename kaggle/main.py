fa9724cafd61c5485ab01ffa80ec946af0f5775b
Helgi Tomas Gislason <helgso@gmail.com>
2019-01-27 20:04:07 -0500

    Kaggle: Moving logic around and writing into file the predictions on the test set

--- kaggle/main.py
+++ kaggle/main.py
@@ -3,13 +3,13 @@ Based on the following tutorial from the web:
 https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-intermediate/convolutional_neural_network/main.py#L35-L56
 """
 
+import os
 import time
 import datetime
 
 import torch 
 import torch.nn as nn
 from torch.optim.lr_scheduler import MultiStepLR
-import numpy as np
 
 import kaggle.neuralnets as neuralnets
 import kaggle.utils as utils
@@ -21,7 +21,7 @@ def main():
 
     print("Using device:", device)
     # Hyper parameters
-    num_epochs = 2
+    num_epochs = 1
     num_classes = 2
     batch_size = 64
     learning_rate = 0.001
@@ -48,110 +48,109 @@ def main():
 
     scheduler = MultiStepLR(optimizer, milestones=[20, 40], gamma=0.1)
 
-    # Train the model
-    total_step = len(train_loader)
-
     print("Training...")
+    total_step = len(train_loader)
     for epoch in range(num_epochs):
-        # Reset metrics for each epoch
-        scheduler.step()
-        full_train_predicted = []
-        full_train_labels = []
-        full_validation_predicted = []
-        full_validation_labels = []
+        train(model, device, total_step, scheduler, train_loader, validation_loader, criterion, optimizer, batch_size, logfile, epoch, num_epochs)
+        validation_accuracy = validate(model, validation_loader, device, logfile)
 
-        correct = 0
-        total = 0
-        for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device, dtype=torch.float)
-            labels = labels.to(device, dtype=torch.long)
+        print("Epoch {} validation accuracy= {:.4f}".format(epoch + 1, validation_accuracy))
+        print("Epoch {} validation accuracy= {:.4f}".format(epoch + 1, validation_accuracy), file=logfile)
 
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            total += labels.size(0)
-            _, predicted = torch.max(outputs.data, 1)
-            full_train_predicted += predicted.cpu().numpy().tolist()
-            full_train_labels += labels.cpu().numpy().tolist()
-
-            correct += (predicted == labels).sum().item()
-
-            if (i+1) % (batch_size/2) == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy:{:.2f}'
-                       .format(epoch+1, num_epochs, i+1, total_step, loss.item(), correct/total), file=logfile)
-                logfile.flush()
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy:{:.2f}'
-                       .format(epoch+1, num_epochs, i+1, total_step, loss.item(), correct/total))
-        utils.print_score("Training", full_train_labels, full_train_predicted, logfile)
-        # At the end of the epoch, perform validation.
         if epoch % 10 == 0:
             torch.save(model, "model.bak")
-            # model.save("model.bak") #Model backup at each 10 epoch, just in case.
 
-        with torch.no_grad():
-            validation_correct = 0
-            validation_total = 0
-            for images, labels in validation_loader:
-                validation_images = images.to(device)
-                validation_labels = labels.to(device)
-                validation_outputs = model(validation_images)
-                _, validation_predicted = torch.max(validation_outputs.data, 1)
-                validation_total += validation_labels.size(0)
-                validation_correct += (validation_predicted == validation_labels).sum().item()
-                full_validation_predicted += validation_predicted.cpu().numpy().tolist()
-                full_validation_labels += validation_labels.cpu().numpy().tolist()
-            utils.print_score("Validation", full_validation_predicted, full_validation_labels, logfile)
-            print("Epoch {} validation accuracy= {:.4f}".format(epoch+1, validation_correct/validation_total))
-            print("Epoch {} validation accuracy= {:.4f}".format(epoch+1, validation_correct/validation_total), file=logfile)
+    print("Training complete.")
 
-    # Test the model. Set it into evaluation mode.
-    model.eval()
-
-    w, h = 10, 10
-    confusion_matrix = [[0 for x in range(w)] for y in range(h)]
-
-    full_test_predicted = []
-    full_test_labels = []
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in test_loader:
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-            predicted = predicted.cpu().numpy().tolist()
-            labels = labels.cpu().numpy().tolist()
-            full_test_predicted += predicted
-            full_test_labels += labels
-
-            # Builds the confusion matrix
-            for prediction, target in zip(predicted, labels):
-                confusion_matrix[prediction][target] += 1
-        utils.print_score("Test", full_test_predicted, full_test_labels, logfile)
-        print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total), file=logfile)
-        print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+    predict_test_set_labels(model, test_loader, device)
 
     # Save the model checkpoint
     torch.save(model.state_dict(), 'model.ckpt')
 
-    np.savetxt("results\\"+logfile_prefix+"_confusion_matrix.txt", np.matrix(confusion_matrix))
 
-    # Just for the log
-    print("Predicted (row) labels vs targets (column)", file=logfile)
-    for i in range(0, 10):
-        for j in range(0, 10):
-            print(confusion_matrix[i][j], "\t", end='', file=logfile)
-        print("\n", end="", file=logfile)
+def train(model, device, total_step, scheduler, train_loader, validation_loader, criterion, optimizer, batch_size, logfile, epoch, num_epochs):
+    # Reset metrics for each epoch
+    scheduler.step()
+    full_train_predicted = []
+    full_train_labels = []
 
-    logfile.close()
+    correct = 0
+    total = 0
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device, dtype=torch.float)
+        labels = labels.to(device, dtype=torch.long)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        total += labels.size(0)
+        _, predicted = torch.max(outputs.data, 1)
+        full_train_predicted += predicted.cpu().numpy().tolist()
+        full_train_labels += labels.cpu().numpy().tolist()
+
+        correct += (predicted == labels).sum().item()
+
+        if (i + 1) % (batch_size / 2) == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy:{:.2f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), correct / total), file=logfile)
+            logfile.flush()
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy:{:.2f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), correct / total))
+
+    utils.print_score("Training", full_train_labels, full_train_predicted, logfile)
+
+
+def validate(model, validation_loader, device, logfile):
+    full_validation_predicted = []
+    full_validation_labels = []
+
+    with torch.no_grad():
+        validation_correct = 0
+        validation_total = 0
+        for images, labels in validation_loader:
+            validation_images = images.to(device, dtype=torch.float)
+            validation_labels = labels.to(device, dtype=torch.long)
+
+            validation_outputs = model(validation_images)
+            _, validation_predicted = torch.max(validation_outputs.data, 1)
+            validation_total += validation_labels.size(0)
+            validation_correct += (validation_predicted == validation_labels).sum().item()
+            full_validation_predicted += validation_predicted.cpu().numpy().tolist()
+            full_validation_labels += validation_labels.cpu().numpy().tolist()
+
+        utils.print_score("Validation", full_validation_predicted, full_validation_labels, logfile)
+
+    return validation_correct / validation_total
+
+
+def predict_test_set_labels(model, test_loader, device):
+    # Test the model. Set it into evaluation mode.
+    model.eval()
+
+    print("Predicting test set labels...")
+    predictions = []
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.to(device, dtype=torch.float)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            predicted = predicted.cpu().numpy().tolist()
+            predictions += predicted
+
+    target_names = ["Dog", "Cat"]
+
+    predictions_file_prefix = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H_%M_%S')
+    predictions_file_path = os.path.join("predictions", predictions_file_prefix+".csv")
+    print("Saving predictions to {}".format(predictions_file_path))
+    with open(predictions_file_path, "w+") as predictions_file:
+        predictions_file.write("id,label\n")
+        for i in range(len(predictions)):
+            predictions_file.write("{},{}\n".format(str(i), target_names[predictions[i]]))
 
 
 if __name__ == '__main__':
